8e6d10d797070934d6d62f23d6ca72165c9d41e5
Pooptassium <mrbeastyturtle@gmail.com>
2025-08-05 19:58:21 -0400

    Sentiment

--- TextAnalysis/views.py
+++ TextAnalysis/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from textblob import TextBlob
 
 # Create your views here.
 def home(request):
@@ -7,8 +8,18 @@ def home(request):
 def result(request):
     textInput = request.POST["text"]
 
+    chars = len(textInput)
+
+    words = len(textInput.split(" "))
+
+    blob = TextBlob(textInput)
+    sentiment = blob.sentiment
+
     context = {
-        "textResult" : textInput
+        "textResult" : textInput,
+        "char_count" : chars,
+        "word_count" : words,
+        "sentiment" : sentiment
     }
 
     return render(request, "textanalysis/result.html", context)
