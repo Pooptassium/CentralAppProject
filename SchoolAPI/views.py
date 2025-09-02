b8f3b0b64b901a23a938ff0121a75f400f470237
Pooptassium <mrbeastyturtle@gmail.com>
2025-09-02 19:26:53 -0400

    Fixed typos

--- SchoolAPI/views.py
+++ SchoolAPI/views.py
@@ -7,7 +7,8 @@ def home(request):
 
 def servercall(request):
     
-    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Schools/FeatureServer/0/query?outFields=*&where=1%3D1"
+    url = "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Schools/FeatureServer/0/query?where=1%3D1&objectIds=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&collation=&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnTrueCurves=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token="
+
 
     response = requests.get(url, verify = False)
 
@@ -18,7 +19,7 @@ def servercall(request):
     schools = []
 
     for record in data["features"]:
-        schools.appened([
+        schools.append([
             record["attributes"]["school_name"],
             record["attributes"]["grade_level"]
         ])
@@ -27,7 +28,7 @@ def servercall(request):
         "schools" : schools
     }
 
-    return render(request, "schoolapi/servercall.hhtml", context)
+    return render(request, "schoolapi/servercall.html", context)
 
 def clientcall(request):
     return render(request, "schoolapi/clientcall.hhtml")
