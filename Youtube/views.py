fc35aacbf2e48b9926de4116d5ce38ea1f2e9ae7
Pooptassium <mrbeastyturtle@gmail.com>
2025-09-09 20:04:35 -0400

    a

--- Youtube/views.py
+++ Youtube/views.py
@@ -1,12 +1,29 @@
 from django.shortcuts import render
+import requests
+import json
 
 # Create your views here.
 def home(request):
-    return render(request, "youtube/home.html'")
+    return render(request, "youtube/home.html")
 
 def servercall(request):
     key = request.POST["text"]
 
+    url = "https://googleapis.com/youtuve/v3/videos"
+
+    params = {
+        "part" : "snippet,statistics",
+        "chart" : "mostPopular",
+        "regionCode" : "US",
+        "maxResults" : 10,
+        "key" : key
+    }
+
+    response = requests.get(url, params)
+
+    print(response)
+
+    data = response.json
     context = {
         "key": key,
     }
